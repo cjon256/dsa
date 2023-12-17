172927d477c88b53baf83dd8af7995bb0aa5d0df
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2023-12-17 18:10:40 -0500

    leet 0125 - Added some test cases for 0125.py
    left over from a previous version of the solution

--- leet/0125.py
+++ leet/0125.py
@@ -12,13 +12,60 @@ class Solution:
 
 class TestSolution(unittest.TestCase):
     def test_isPalindrome(self):
+        solution = Solution()
+
+        # Test case 1: Empty string
+        self.assertTrue(solution.isPalindrome(""))
+
+        # Test case 2: Single character
+        self.assertTrue(solution.isPalindrome("a"))
+
+        # Test case 3: Palindrome with even length
+        self.assertTrue(solution.isPalindrome("racecar"))
+
+        # Test case 4: Palindrome with odd length
+        self.assertTrue(solution.isPalindrome("level"))
+
+        # Test case 5: Non-palindrome
+        self.assertFalse(solution.isPalindrome("hello"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+class TestSolution(unittest.TestCase):
+    def test_isPalindrome(self):
         self.assertTrue(Solution().isPalindrome(""))
         self.assertTrue(Solution().isPalindrome("a"))
         self.assertTrue(Solution().isPalindrome("racecar"))
         self.assertTrue(Solution().isPalindrome("level"))
         self.assertTrue(Solution().isPalindrome(
+
+
+class TestSolution(unittest.TestCase):
+    def test_isPalindrome(self):
+        solution = Solution()
+
+        # Test case 1: Empty string
+        self.assertTrue(solution.isPalindrome(""))
+
+        # Test case 2: Single character
+        self.assertTrue(solution.isPalindrome("a"))
+
+        # Test case 3: Palindrome with even length
+        self.assertTrue(solution.isPalindrome("racecar"))
+
+        # Test case 4: Palindrome with odd length
+        self.assertTrue(solution.isPalindrome("level"))
+
+        # Test case 5: Non-palindrome
+        self.assertFalse(solution.isPalindrome("hello"))
+
+        # Test case 6: Palindrome with special characters
+        self.assertTrue(solution.isPalindrome(
             "A man, a plan, a canal: Panama"))
-        self.assertFalse(Solution().isPalindrome("hello"))
+
+        # Test case 7: Non-palindrome with special characters
+        self.assertFalse(solution.isPalindrome("race a car"))
 
 
 if __name__ == '__main__':
