2a468242ea5be3cc1a9f222266598c81a2f541d0
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-22 17:34:04 -0500

    0409 what if I just count the odd ones?

--- leet/0409.py
+++ leet/0409.py
@@ -4,14 +4,18 @@ from collections import Counter
 
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        ctr = Counter(s)
-        result = 0
-        at_least_one_odd = False
-        for v in ctr.values():
-            result += v // 2
-            if not at_least_one_odd and v % 2 == 1:
-                at_least_one_odd = True
-        return result * 2 + 1 if at_least_one_odd else result * 2
+        def count_odd(s: str) -> int:
+            ctr = Counter(s)
+            result = 0
+            for v in ctr.values():
+                if v % 2 == 1:
+                    result += 1
+            return result
+
+        num_odd = count_odd(s)
+        if num_odd == 0:
+            return len(s)
+        return len(s) - num_odd + 1
 
 
 class TestSolution(unittest.TestCase):
