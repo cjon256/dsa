02dfccc624e4ad32637d34d60305022c7ea78633
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-02-07 17:46:04 -0500

    Update 0206.reverse-linked-list.py

--- python/0206.reverse-linked-list.py
+++ python/0206.reverse-linked-list.py
@@ -1,18 +1,56 @@
-# pylint: disable=invalid-name, missing-class-docstring, missing-function-docstring, missing-module-docstring
-# pylint: disable=too-few-public-methods
+#  Category: algorithms
+#  Level: Easy
+#  Percent: 75.53116%
+# pylint: enable=useless-suppression
+# pylint: disable=invalid-name, line-too-long, too-few-public-methods
+# pylint: disable=missing-class-docstring, missing-function-docstring, missing-module-docstring
+
+
+#  Given the head of a singly linked list, reverse the list, and return the reversed list.
+#
+#
+#  Example 1:
+#
+#  Input: head = [1,2,3,4,5]
+#  Output: [5,4,3,2,1]
+#
+#
+#  Example 2:
+#
+#  Input: head = [1,2]
+#  Output: [2,1]
+#
+#
+#  Example 3:
+#
+#  Input: head = []
+#  Output: []
+#
+#
+#
+#  Constraints:
+#
+#
+#  	The number of nodes in the list is the range [0, 5000].
+#  	-5000 <= Node.val <= 5000
+#
+#
+#
+#  Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
 import unittest
-from dataclasses import dataclass
-from typing import Optional, Self
+from typing import Optional
 
-from leetopenlib.linked_list import linked_list_to_list, list_to_linked_list
-
-
-@dataclass
-class ListNode:
-    val: int
-    next: Optional[Self] = None
+from leetopenlib.linked_list import ListNode, linked_list_to_list, list_to_linked_list
 
 
+#  start_marker
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
@@ -27,9 +65,39 @@ class Solution:
             head = tmp
         head.next = curr
         return head
+        #  end_marker
 
 
-#  end_marker
+class OldSolution:
+    def reverseList_recursive(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse_list_r(prev: ListNode, curr: ListNode) -> ListNode:
+            if curr is None:
+                return prev
+            curr_next = curr.next
+            curr.next = prev
+            return reverse_list_r(curr, curr_next)
+
+        if head is None:
+            return head
+        child = head.next
+        if child is None:
+            return head
+        head.next = None
+        child_next = child.next
+        child.next = head
+        return reverse_list_r(child, child_next)
+
+    def reverseList_iter(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            child = curr.next
+            curr.next = prev
+            prev = curr
+            curr = child
+        return prev
+
+
 class TestSolution(unittest.TestCase):
     def test_1(self):
         s = Solution()
