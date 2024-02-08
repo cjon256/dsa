ae205d9faa21534206a8d547953db689b4da93e4
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-02-07 19:12:06 -0500

    Update 0409.longest-palindrome.py

--- python/0409.longest-palindrome.py
+++ python/0409.longest-palindrome.py
@@ -1,7 +1,43 @@
+#  Category: algorithms
+#  Level: Easy
+#  Percent: 53.8822%
+# pylint: enable=useless-suppression
+# pylint: disable=invalid-name, line-too-long, too-few-public-methods
+# pylint: disable=missing-class-docstring, missing-function-docstring, missing-module-docstring
+
+
+#  Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+#
+#  Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+#
+#
+#  Example 1:
+#
+#  Input: s = "abccccdd"
+#  Output: 7
+#  Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+#
+#
+#  Example 2:
+#
+#  Input: s = "a"
+#  Output: 1
+#  Explanation: The longest palindrome that can be built is "a", whose length is 1.
+#
+#
+#
+#  Constraints:
+#
+#
+#  	1 <= s.length <= 2000
+#  	s consists of lowercase and/or uppercase English letters only.
+#
+
 import unittest
 from collections import Counter
 
 
+#  start_marker
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         def count_odd(s: str) -> int:
@@ -16,6 +52,7 @@ class Solution:
         if num_odd == 0:
             return len(s)
         return len(s) - num_odd + 1
+        #  end_marker
 
 
 class TestSolution(unittest.TestCase):
