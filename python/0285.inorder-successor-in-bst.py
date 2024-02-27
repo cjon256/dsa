4d30baeccc23a29ac8d686b2852e3122544e0d78
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-02-27 18:29:38 -0500

    0285.inorder-successor-in-bst.py playing a bit with alt solution

--- python/0285.inorder-successor-in-bst.py
+++ python/0285.inorder-successor-in-bst.py
@@ -159,22 +159,26 @@ if __name__ == "__main__":
 
         successor = None
 
+        print(f"root: {root.val if root else None} successor: {successor.val if successor else None}")  # fmt: skip
         while root:
-            print(f"root: {root.val}")
 
             if p.val >= root.val:
+                print("root.right")
                 root = root.right  # type: ignore
             else:
+                print("root.left")
                 successor = root
                 root = root.left  # type: ignore
-
+            print(f"root: {root.val if root else None} successor: {successor.val if successor else None}")  # fmt: skip
         return successor  # type: ignore
 
     # fmt: off
     root = list_to_tree( [ 41, 37, 44, 24, 39, 42, 48, 1, 35, 38, 40, null, 43, 46, 49, 0, 2, 30, 36, null, null, null, null, null, null, 45, 47, null, null, null, null, null, 4, 29, 32, null, null, null, null, null, null, 3, 9, 26, null, 31, 34, null, null, 7, 11, 25, 27, null, null, 33, null, 6, 8, 10, 16, null, null, null, 28, null, null, 5, null, null, null, null, null, 15, 19, null, null, null, null, 12, null, 18, 20, null, 13, 17, null, null, 22, null, 14, null, null, 21, 23, ])
     # fmt: on
-    p = find_node_in_bst(root, 40)
-    expected = 41
+    p = find_node_in_bst(root, 41)
     result = inorderSuccessor(root, p)  # type: ignore
-    print(result.val, expected)
-    root.pp_rev()
+    if result:
+        print(result.val)
+    else:
+        print(result)
+    # root.pp()
