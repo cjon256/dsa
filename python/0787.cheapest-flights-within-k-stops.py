80e1a41d2f9b9014b2c39f2c3d31291d9087d1a1
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-02-29 22:21:45 -0500

    0787.cheapest-flights-within-k-stops.py
    honestly don't know what this is... is this bellman-ford maybe?

--- python/0787.cheapest-flights-within-k-stops.py
+++ python/0787.cheapest-flights-within-k-stops.py
@@ -4,8 +4,8 @@
 # pylint: enable=useless-suppression
 # pylint: disable=invalid-name, line-too-long, too-few-public-methods
 # pylint: disable=missing-class-docstring, missing-function-docstring, missing-module-docstring
-
-
+#
+#
 #  There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
 #
 #  You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
@@ -68,7 +68,8 @@ class Solution:
     ) -> int:
         Flt = namedtuple("Flt", "cost hops dest")
         Hop = namedtuple("Hop", "cost city")
-        visited = [False] * n
+        distances = [[None] * n for _ in range(k + 1)]
+        min_dist = 2**31
         next_hops_dict: defaultdict[int, List[Hop]] = defaultdict(lambda: [])
         for flight in flights:
             from_, to, price = flight
@@ -77,21 +78,30 @@ class Solution:
         heap = [init]
         while heap:
             np = heapq.heappop(heap)
-            remaining_hops = k - np.hops + 1
+            if np.hops - 1 > k:
+                continue
             if np.dest == dst:
-                return np.cost
-            if remaining_hops == 0:
-                continue
-            if visited[np.dest]:
-                continue
-            visited[np.dest] = True
+                min_dist = min(min_dist, np.cost)
             next_hops: List[Hop] = next_hops_dict[np.dest]
             for hop in next_hops:
-                if hop.city != dst and remaining_hops == 1:
+                if hop.city != dst and np.hops + 1 > k:
                     continue
-                heapq.heappush(heap, Flt(np.cost + hop.cost, np.hops + 1, hop.city))
+                if np.hops <= k:
+                    if distances[np.hops][hop.city] is None:
+                        distances[np.hops][hop.city] = np.cost + hop.cost
+                    elif np.cost + hop.cost >= distances[np.hops][hop.city]:
+                        continue
+                    else:
+                        distances[np.hops][hop.city] = np.cost + hop.cost
+                    if hop.city == dst:
+                        min_dist = min(min_dist, distances[np.hops][hop.city])
+                if np.hops < k:
+                    heapq.heappush(heap, Flt(np.cost + hop.cost, np.hops + 1, hop.city))
 
-        return -1
+        if min_dist == 2**31:
+            min_dist = -1
+
+        return min_dist
         #  end_marker
 
 
@@ -144,6 +154,18 @@ class TestSolution(unittest.TestCase):
         dst = 2
         k = 2
         expected = 7
+        result = Solution().findCheapestPrice(n, flights, src, dst, k)
+        self.assertEqual(result, expected)
+
+    def test_case_6(self):
+        n = 17
+        # fmt: off
+        flights = [[0,12,28],[5,6,39],[8,6,59],[13,15,7],[13,12,38],[10,12,35],[15,3,23],[7,11,26],[9,4,65],[10,2,38],[4,7,7],[14,15,31],[2,12,44],[8,10,34],[13,6,29],[5,14,89],[11,16,13],[7,3,46],[10,15,19],[12,4,58],[13,16,11],[16,4,76],[2,0,12],[15,0,22],[16,12,13],[7,1,29],[7,14,100],[16,1,14],[9,6,74],[11,1,73],[2,11,60],[10,11,85],[2,5,49],[3,4,17],[4,9,77],[16,3,47],[15,6,78],[14,1,90],[10,5,95],[1,11,30],[11,0,37],[10,4,86],[0,8,57],[6,14,68],[16,8,3],[13,0,65],[2,13,6],[5,13,5],[8,11,31],[6,10,20],[6,2,33],[9,1,3],[14,9,58],[12,3,19],[11,2,74],[12,14,48],[16,11,100],[3,12,38],[12,13,77],[10,9,99],[15,13,98],[15,12,71],[1,4,28],[7,0,83],[3,5,100],[8,9,14],[15,11,57],[3,6,65],[1,3,45],[14,7,74],[2,10,39],[4,8,73],[13,5,77],[10,0,43],[12,9,92],[8,2,26],[1,7,7],[9,12,10],[13,11,64],[8,13,80],[6,12,74],[9,7,35],[0,15,48],[3,7,87],[16,9,42],[5,16,64],[4,5,65],[15,14,70],[12,0,13],[16,14,52],[3,10,80],[14,11,85],[15,2,77],[4,11,19],[2,7,49],[10,7,78],[14,6,84],[13,7,50],[11,6,75],[5,10,46],[13,8,43],[9,10,49],[7,12,64],[0,10,76],[5,9,77],[8,3,28],[11,9,28],[12,16,87],[12,6,24],[9,15,94],[5,7,77],[4,10,18],[7,2,11],[9,5,41]]
+        # fmt: on
+        src = 13
+        dst = 4
+        k = 13
+        expected = 47
         result = Solution().findCheapestPrice(n, flights, src, dst, k)
         self.assertEqual(result, expected)
 
